19b5ac859ebe2c566ffc296d1fd9db5edb5c1e4c
Andris Adumans <Andris_Adumans@rcomext.com>
2019-08-23 17:49:52 +0300

    Practise lesson 8

---
+++ school_project/school_practice/src/main/java/Practice99.java
@@ -0,0 +1,19 @@
+/*
+ * @(#)Practice99.java
+ *
+ * Copyright  Andris Adumans Riga, Latvia. All rights reserved.
+ */
+
+public class Practice99 {
+
+
+    public static void main(String[] args) {
+        for (int i = 0; i< args.length; i++){
+            System.out.println(i);
+            System.out.println(args[i]);
+        }
+    }
+
+
+
+}
