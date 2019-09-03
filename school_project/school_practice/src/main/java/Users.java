695e2ecd116480b028929bb4c43f7f1d17168758
Andris Adumans <Andris_Adumans@rcomext.com>
2019-09-03 17:45:48 +0300

    School 03.09.2019.

---
+++ school_project/school_practice/src/main/java/Users.java
@@ -0,0 +1,73 @@
+/*
+ * @(#)Users.java
+ *
+ * Copyright  Andris Adumans Riga, Latvia. All rights reserved.
+ */
+
+public class Users {
+
+    private int id;
+    private String name;
+    private String username;
+    private String email;
+    private Object adress;
+    private String phone;
+    private String website;
+    private Company company;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Object getAdress() {
+        return adress;
+    }
+
+    public void setAdress(Object adress) {
+        this.adress = adress;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+}
---
+++ school_project/school_practice/src/test/java/SandBoxTests.java
@@ -0,0 +1,46 @@
+/*
+ * @(#)SandBoxTests.java
+ *
+ * Copyright  Andris Adumans Riga, Latvia. All rights reserved.
+ */
+
+import static io.restassured.RestAssured.given;
+import static org.testng.AssertJUnit.assertEquals;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SandBoxTests {
+
+    private static RequestSpecification spec;
+
+    @BeforeAll
+    public static void init() {
+        spec = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setProxy("gate.swissre.com", 9443)
+                .setBaseUri("https://jsonplaceholder.typicode.com")
+                .addFilter(new ResponseLoggingFilter())
+                .addFilter(new RequestLoggingFilter())
+                .build();
+    }
+
+    @Test
+    public void simpleBackEndTest(){
+        Users[] users  = given()
+                .spec(spec)
+                .when()
+                .get("users")
+                .then()
+                .statusCode(200)
+                .extract().as(Users[].class);
+
+        assertEquals("Hoba", users[2].getName());
+
+    }
+}
